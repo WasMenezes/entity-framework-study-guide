608d9da83716ba12d88e658bea9b622fa739b0bd
wasmenezes <was288519@gmail.com>
2021-06-05 03:14:13 -0300

    Criando classes de entidades

---
+++ 01 - dominando ef core/Curso/Domain/Departamento.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DominandoEFCore.Domain
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+        public List<Funcionario> Funcionarois { get; set; }
+    }
+}
