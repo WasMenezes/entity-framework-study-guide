b247af0373cee8059e52b4177a8609e6016bcd55
wasmenezes <was288519@gmail.com>
2021-06-05 03:31:29 -0300

    Resolvendo GAP do EnsureCreated para múltiplos contextos

--- 01 - dominando ef core/Curso/Program.cs
+++ 01 - dominando ef core/Curso/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
 
 namespace DominandoEFCore
 {
@@ -6,7 +8,7 @@ namespace DominandoEFCore
     {
         static void Main(string[] args)
         {
-            EnsureCreatedAndDelete();
+            GapDoEnsureCreated();
         }
 
         static void EnsureCreatedAndDelete()
@@ -14,7 +16,18 @@ namespace DominandoEFCore
             using var db = new Data.ApplicationContext();
             //db.Database.EnsureCreated();
             db.Database.EnsureDeleted();
+        }
 
+        static void GapDoEnsureCreated()
+        {
+            using var db1 = new Data.ApplicationContext();
+            using var db2 = new Data.ApplicationContextCidade();
+
+            db1.Database.EnsureCreated();
+            db2.Database.EnsureCreated();
+
+            var databaseCreator = db2.GetService<IRelationalDatabaseCreator>();
+            databaseCreator.CreateTables();
         }
     }
 }
